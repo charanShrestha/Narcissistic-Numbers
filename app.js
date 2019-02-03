80f31887e402fe92292ff934bb6bbee7ec42a4af
charanShrestha <44975485+charanShrestha@users.noreply.github.com>
2019-02-03 22:31:26 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+function isNarcissistic(n) {
+  const strgNum =  n.toString();
+  const indivNum = strgNum.split('');
+  let sum = 0;
+  for (let i = 0; i < a.length; i++) {
+    sum = sum + Math.pow(indivNum[i],a.length); 
+  }
+  if (sum === n) return true;
+  else return false;
+}
